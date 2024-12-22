fba271ede10e5e7297620a65f2f39de81fcd20c2
Dmitrii Akimov <akimov@1060.ru>
2024-12-22 12:05:07 +0300

    Added user info to show_one_party log message

--- bot/utils/log.py
+++ bot/utils/log.py
@@ -1,12 +1,13 @@
 from telegram.ext import ContextTypes
+from telegram import User
 
 
-def log_entry_with_several_lines(game: str, day: str, context: ContextTypes.DEFAULT_TYPE) -> str:
+def log_entry_with_several_lines(user: User, game: str, day: str, context: ContextTypes.DEFAULT_TYPE) -> str:
     users = context.chat_data[day][game]
-    text = f'Showing `{game}` party on  `{day}`'
-    for user, time_dict in users.items():
+    text = f'Showing `{game}` party on `{day}` to {user}'
+    for user, ready_time in users.items():
         text += '\n'
         text += f'{" " * 76}{user}: '
-        text += ' '.join([f'{key}={value}' for key, value in time_dict.items()])
+        text += ' '.join([f'{key}={value}' for key, value in ready_time.__dict__.items()])
 
     return text
